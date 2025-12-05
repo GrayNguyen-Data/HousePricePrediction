ffe038bb6c29b67a1492d09da4e6c2aa10abcf17
GrayNguyen77 <ndtoan.work@gmail.com>
2025-12-06 01:53:07 +0700

    Outlier processing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 from src.data_collection.crawler_bds import crawler_and_load_data
-
+from database.connect_db import connect
 
 def main():
-    # crawler_and_load_data()
+    crawler_and_load_data()
 
 if __name__ == "__main__":
     main()
